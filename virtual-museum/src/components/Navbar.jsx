5aa9be8c962bdf5d6f280231d8394f4baee39bf6
Monika Phuengmak <monika.phuengmak@gmail.com>
2025-02-22 23:53:30 -0500

    Add logo to navbar

--- virtual-museum/src/components/Navbar.jsx
+++ virtual-museum/src/components/Navbar.jsx
@@ -1,16 +1,17 @@
+import logo from "../assets/logo.png";
+
 export default function Navbar() {
     return (
         <nav class="navbar navbar-expand-lg bg-body-tertiary w-100">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/">U.S. Plant Museum</a>
+                <a class="navbar-brand d-flex align-items-center" href="/">
+                    <img src={logo} alt="Logo" width="60px" height="60px" class="d-inline-block align-text-top" />
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Home</a>
-                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Link</a>
                     </li>
