5d573eb9aeffc2b4015f6319b967b397cb319d4e
annaandmandy <annaandtommy1004@gmail.com>
2025-02-22 23:46:26 -0500

    home

--- virtual-museum/src/pages/Home.jsx
+++ virtual-museum/src/pages/Home.jsx
@@ -1,13 +1,7 @@
 import FeaturedCollection from "../components/FeaturedCollection"
 import USMap from "../components/USMap";
-<<<<<<< HEAD
 import MysteryBox from "../components/mysteryBox";
 
 export default function Home() {
     return <><FeaturedCollection location="Massachusetts" /><USMap /><MysteryBox /></>;
-=======
-
-export default function Home() {
-    return <><FeaturedCollection location="Massachusetts" /><USMap /></>;
->>>>>>> origin
 }
